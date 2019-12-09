70fd6566ef9a5be80a1c3c81e0336fc4d322bb7b
Scott Ladd <scottjr101@gmail.com>
2019-12-09 16:17:23 -0500

    Altered setup

--- models/litty.js
+++ models/litty.js
@@ -1,7 +1,25 @@
-module.exports = function(sequelize, DataTypes) {
-  var Litty = sequelize.define("Litty", {
-    name: DataTypes.STRING,
-    message: DataTypes.TEXT
-  });
-  return Litty;
-};
+const Sequelize = require('sequelize');
+
+var Litty = sequelize.define('Litty', {
+  // attributes
+  Name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  Message: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+}, {
+  // options
+});
+
+module.exports = Litty;
+
+// module.exports = function(sequelize, DataTypes) {
+//   var Litty = sequelize.define("Litty", {
+//     name: DataTypes.STRING,
+//     message: DataTypes.TEXT
+//   });
+//   return Litty;
+// };
