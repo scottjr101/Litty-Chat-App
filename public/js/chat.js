8dbb1a51b7a2e5f3d8ca94db273cb4dcc6534b3c
Foster Gailey <foster@gaileyhome.com>
2019-12-11 18:56:09 -0500

    small changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -49,7 +49,7 @@ message.addEventListener('keypress', function(){
 // Listen for events
 socket.on('chat', function(data){
     feedback.innerHTML = '';
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    output.innerHTML = '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>' + output.innerHTML;
     console.log('chat function works')
 });
 
